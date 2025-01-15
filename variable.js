aa6cb3b91c82316cb022d31b6e2c983038dc4889
radiyaeshi <eshi50dishi@gmail.com>
2025-01-15 21:38:50 +0600

    we know

--- variable.js
+++ variable.js
@@ -1,8 +1,15 @@
 var boxOne;
 boxOne="Salt";
-console.log(boxOne);
+console.log(boxOne, "This is line from 2");
 var boxTwo;
 boxTwo="Sugar";
-console .log()
+console.log(boxTwo,"This is line from 6");
 var box3;
 box3="Chilli";
+console.log(box3);
+var girlsname;
+girlsname="jamila"
+console.log(girlsname,"before marriage")
+girlsname="jamila yousuf"
+console.log(girlsname,"after marriage")
+
